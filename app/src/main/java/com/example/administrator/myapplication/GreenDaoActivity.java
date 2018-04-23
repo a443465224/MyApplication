c5e46aa261e09d3210b8c2db485b6009bb2b6c6d
xdh <xdh443465224@163.com>
2018-04-23 14:08:37 +0800

    删除空格。

--- app/src/main/java/com/example/administrator/myapplication/GreenDaoActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/GreenDaoActivity.java
@@ -18,10 +18,6 @@ public class GreenDaoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lambda);
-
-
-
-
         img = (ImageView) findViewById(R.id.imageView2);
         img.post(() -> {
             LogUtils.e("",img.getWidth(),img.getHeight(),img.getDrawable().getIntrinsicWidth(),img.getDrawable().getIntrinsicHeight()
